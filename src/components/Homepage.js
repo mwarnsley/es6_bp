01814bcc4aba7899e00551cede32bdef8350417d
mwarnsley <marcus.warnsley@gmail.com>
2016-02-02 23:40:23 -0500

    adding more react and bootstrap for the editing of the cards

--- src/components/Homepage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class Homepage extends React.Component{
-	render(){
-		return <div>
-			My first es6 React Component
-		</div>
-	}
-}
--- src/main.js
+++ src/main.js
@@ -4,4 +4,12 @@ const message = () => {
 	return 'Hello, I\'m now doing es6!!!';
 }
 
-console.log(message());	
+console.log(message());
+
+class Athlete {
+	constructor(){
+		console.log("I am an athlete");
+	}
+}
+
+var wayne = new Athlete();
